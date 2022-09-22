c5071bf00b459f4c73c7cc874e13a08fa0492870
Mahananda2000 <mahananda.xworkz@gmail.com>
2022-09-22 13:07:59 +0530

    commiting java file

---
+++ User.java
@@ -0,0 +1,10 @@
+class User {
+   
+     public static void main(String don[]){
+     String name = "Maha";
+     int age = 22;
+     System.out.println("Username is"+ name);
+     System.out.println("User age is" + age);
+  
+  }
+}
